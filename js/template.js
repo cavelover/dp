4bf888cae02a0d04f714162801af01fbefb6972f
shun021677@gmail.com <shun021677@gmail.com>
2021-05-18 21:03:10 +0900

    test

--- js/template.js
+++ js/template.js
@@ -2,6 +2,6 @@ function head(title){
     var html = "";
     html += '<title>' + title + '</title>';
     html += '<script>MathJax = {chtml:{matchFontHeight: false},tex:{inlineMath: [["$", "$"]]}};</script>'
-    html += ' <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>'
+    html += '<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>'
     document.write(html);
     }
